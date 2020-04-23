bc83002e1a92a2d201bee4e3f4a8be5f03d41569
Leandro Rocha <prog.leandro@gmail.com>
2020-04-22 23:30:37 -0300

    Configurando a estrutura

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello world.'})
+})
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+  
+});
